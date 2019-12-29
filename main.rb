4745aa472f29151c68f8f2aae3f7a4c6add3b6e0
ricardosikic <ricardo.sikic@gmail.com>
2019-12-29 01:05:47 +0000

    added encapsulation + inherance

--- main.rb
+++ main.rb
@@ -27,3 +27,47 @@ end
 
 calculo_1 = Calculo.new(4, 3)
 puts calculo_1.suma.resta
+
+
+
+class Manager 
+  attr_accessor :name, :salary
+
+  def initialize(name, salary)
+   @name = name
+   @salary = salary
+  end
+  
+  def information
+   puts "worker name #{@name} have the salary #{@salary}"
+  end
+
+  # Existen 3 tipos de encapsulamiento
+  # public, protected, private
+  # para este ejemplo usaremos el protected
+
+  protected
+  
+  def get_payment
+    return (@salary * 1.15)
+  end
+
+end
+
+
+class Cashier < Manager
+  attr_accessor :bonus
+
+  def initialize(name, salary, bonus)
+    @bonus = bonus
+    @salary = salary
+    @bonus = bonus
+  end
+
+  def get_payment_cashier
+    return get_payment + @bonus
+  end
+end
+
+cashier_1 = Cashier.new('alex', 300, 105)
+puts cashier_1.get_payment_cashier
